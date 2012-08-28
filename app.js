82bc54685c20afebfbf2583366717892d9a97e6a
t2t2 <taavot@gmail.com>
2012-08-28 17:55:38 +0300

    Base ui, dev tools

--- app.js
+++ app.js
@@ -7,11 +7,23 @@ var app = require("appjs");
 app.serveFilesFrom(__dirname+'/public');
 
 var window = app.createWindow('http://appjs/', {
-	width: 640,
-	height: 460,
+	width: 700,
+	height: 520,
 });
 window.on('create', function(){
 	console.log("Window Created");
 	window.frame.show();
 	window.frame.center();
+});
+window.on('ready', function(){
+	console.log("Window Ready");
+
+	function F12(e){ return e.keyIdentifier === 'F12' }
+	function Command_Option_J(e){ return e.keyCode === 74 && e.metaKey && e.altKey }
+
+	window.addEventListener('keydown', function(e){
+		if (F12(e) || Command_Option_J(e)) {
+			window.frame.openDevTools();
+		}
+	});
 });
